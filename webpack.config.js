11836c2cefb8c82d59fb31a3a5ab9782d3234d09
DimWebDev <liasesdemetrio@gmail.com>
2022-10-25 18:24:41 +0300

    Configuring webpack and webpackl dev server

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,19 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/app.ts",
   mode: "development",
+  devServer: {
+    open: true,
+    historyApiFallback: true,
+
+    static: {
+      directory: path.join(__dirname, "./"),
+    },
+  },
+
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist",
   },
   devtool: false,
   module: {
